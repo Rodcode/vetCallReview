45626201453bc2146498390afaf05a3f2aefe7b6
Drod86 <daniel.r.rodriguez86@gmail.com>
2023-05-15 12:39:01 -0500

    set up scss in webpack

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import VetCallReview from "./components/VetCallReview";
+import "./styles/styles.scss";
 
 ReactDOM.render(<VetCallReview />, document.getElementById("app"));
